033117d7cf128db39901a22b958fa90e08488f3b
Tobias Wallura <tobias@wallura.org>
2018-08-07 17:56:41 +0200

    setup webpack build with TypeScript support

---
+++ webpack.config.ts
@@ -0,0 +1,23 @@
+import * as path from 'path'
+
+const config = {
+  entry: './src/index.ts',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    extensions: [ '.tsx', '.ts', '.js' ]
+  },
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname, 'dist')
+  }
+};
+
+export default config;
