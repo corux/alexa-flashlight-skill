9cdcbaef0fc4f01348a3dcef648986336a5b83ab
osiride <usiride@gmail.com>
2021-07-01 00:27:05 +0200

    Add support for it-IT
    This allows the skill to appear on the amazon.it store: https://developer.amazon.com/en-US/docs/alexa/custom-skills/develop-skills-in-multiple-languages.html#example-worldwide-distribution-with-one-language
    
    The only thing missing is the eTag from .ask\config.

--- src/handlers/AMAZON_HelpIntentHandler.ts
+++ src/handlers/AMAZON_HelpIntentHandler.ts
@@ -15,6 +15,9 @@ export class AmazonHelpIntentHandler implements RequestHandler {
     if (request.locale.match(/^de-/)) {
       helpText = `Dieser Skill lässt den Echo Licht Ring und die Echo Buttons leuchten.
         Für weitere Informationen siehe im Skill Store nach.`;
+    } else if (request.locale.match(/^it-/)) {
+      helpText = `Questa skill fa accendere l'anello luminoso di un Echo o un Echo Button.
+        Per maggiori informazioni visita lo Skill Store`;
     } else {
       helpText = `This Skill lets the Echo light ring and the Echo Buttons glow.
         Further information can be found in the Skill Store.`;
